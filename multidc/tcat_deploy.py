e507a4e0648f47efd8df160c4b50d5f6fc05346c
Collin Poczatek <collin.poczatek@gmail.com>
2017-02-02 12:18:12 -0800

    change stack naming default

--- multidc/tcat_deploy.py
+++ multidc/tcat_deploy.py
@@ -37,7 +37,7 @@ def main():
         tcat_obj.s3upload(tcat_cfg)
         tcat_obj.validate_template(tcat_cfg, test_list)
         tcat_obj.validate_parameters(tcat_cfg, test_list)
-        stackinfo = tcat_obj.stackcreate(tcat_cfg, test_list, 'quickstart-datastax')
+        stackinfo = tcat_obj.stackcreate(tcat_cfg, test_list, 'dse')
         tcat_obj.get_stackstatus(stackinfo , 5)
 
 # --End
