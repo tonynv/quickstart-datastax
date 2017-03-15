1f6c7199cc0406038e6bdfcd7059f42b9560926f
Collin Poczatek <collin.poczatek@gmail.com>
2017-03-14 21:33:28 -0700

    remove t2.medium

--- multidc/deploy.sh
+++ multidc/deploy.sh
@@ -11,7 +11,7 @@ regions=\"us-east-1,us-east-1,us-west-1\"
 dcs=\"dc0,dc1,dc2\"
 dcsizes=\"5,5,4\"
 keys=\"dse-keypair-us-east-1,dse-keypair-us-east-1,dse-keypair-us-west-1\"
-instances=\"m4.large,m4.large,t2.medium\"
+instances=\"m4.large,m4.large,m4.large\"
 volsizes=\"128,128,256\"
 ####################################################
 
