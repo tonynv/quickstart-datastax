84c79256084f38f1cacedc01bcc0b4fd7ef47626
Collin Poczatek <collin.poczatek@gmail.com>
2017-03-20 11:55:38 -0700

    swith to 1 region, pubip->privip arg

--- multidc/deploy.sh
+++ multidc/deploy.sh
@@ -7,12 +7,12 @@ keyname="dse-keypair-us-east-1"
 clustername="mycluster"
 pw="secretpassword"
 # List params
-regions=\"us-east-1,us-east-1,us-west-1\"
-dcs=\"dc0,dc1,dc2\"
-dcsizes=\"5,5,4\"
-keys=\"dse-keypair-us-east-1,dse-keypair-us-east-1,dse-keypair-us-west-1\"
-instances=\"m4.large,m4.large,t2.medium\"
-volsizes=\"128,128,256\"
+regions=\"us-east-1,us-east-1\"
+dcs=\"dc0,dc1\"
+dcsizes=\"3,3\"
+keys=\"dse-keypair-us-east-1,dse-keypair-us-east-1\"
+instances=\"m4.large,m4.large\"
+volsizes=\"128,128\"
 ####################################################
 
 template="$PWD/cfn-opscenter.json"
