5aded537c18e015d14bd55375268aee704549b87
Collin Poczatek <collin.poczatek@gmail.com>
2017-04-14 11:48:09 -0700

    fix path

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-aws cloudformation create-stack  --region us-east-1 --stack-name root-stack  --disable-rollback  --capabilities CAPABILITY_IAM  --template-body file://$(pwd)/main.template  --parameters file://$(pwd)/parameters.json
+aws cloudformation create-stack  \
+--region us-east-1 \
+--stack-name root-stack  \
+--disable-rollback  \
+--capabilities CAPABILITY_IAM  \
+--template-body file://$(pwd)/templates/main.template  \
+--parameters file://$(pwd)/ci/parameters.json
