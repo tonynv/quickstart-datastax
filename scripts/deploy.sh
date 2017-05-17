eedf00c8e2ae12f961ae3b377974b911031ae3fd
Collin Poczatek <collin.poczatek@gmail.com>
2017-05-17 11:22:15 -0700

    delete unused dev/test scripts

--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-aws cloudformation create-stack  \
---region us-east-1 \
---stack-name root-stack  \
---disable-rollback  \
---capabilities CAPABILITY_IAM  \
---template-body file://$(pwd)/templates/main.template  \
---parameters file://$(pwd)/ci/simple-parameters.json
