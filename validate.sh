e689bd3ae912e3e5fcd94cdae22db64e2b48bcc4
Scott Hendrickson <scott.hendrickson@datastax.com>
2019-01-18 13:03:24 -0500

    added OPSC instance type so not hardcoded to t2 series

--- validate.sh
+++ validate.sh
@@ -4,7 +4,7 @@ echo "master"
 aws cloudformation validate-template --template-body file://$(pwd)/templates/quickstart-datastax-master.template
 echo "novpc"
 aws cloudformation validate-template --template-body file://$(pwd)/templates/quickstart-datastax-no-vpc.template
-echo "ddac"
+echo "datastax"
 aws cloudformation validate-template --template-body file://$(pwd)/templates/opscenter.template
 echo "datacenter"
 aws cloudformation validate-template --template-body file://$(pwd)/templates/datacenter.template
