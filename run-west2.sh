c50f2ac571c8abbe3c54a21fa6b110d116f3185b
Scott Hendrickson <scott.hendrickson@datastax.com>
2019-04-19 10:09:12 -0400

    updated quickstart-datastax-no-vpc.template for 6.7.1

--- run-west2.sh
+++ run-west2.sh
@@ -6,4 +6,4 @@ echo "Setting up $bucket"
 aws s3 sync ./templates/ s3://$bucket/quickstart-datastax/templates/
 aws s3 sync ./submodules/quickstart-aws-vpc/templates/ s3://$bucket/quickstart-datastax/submodules/quickstart-aws-vpc/templates/
 
-aws cloudformation create-stack  --region us-west-2 --stack-name scotth-mp-dev  --disable-rollback  --capabilities CAPABILITY_IAM  --template-body file://$(pwd)/templates/quickstart-datastax-master.template  --parameters file://$(pwd)/minimal-west.json
+aws cloudformation create-stack  --region us-west-2 --stack-name scotth-mp-dev  --disable-rollback  --capabilities CAPABILITY_IAM  --template-body file://$(pwd)/templates/quickstart-datastax-master.template  --parameters file://$(pwd)/minimal-west2.json
